0309d4b0b58b24ae20534fbb27bb86e0dccae2be
sanober riaz <sanober@sanober.me>
2018-01-23 23:45:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getFirstSelector(selector){
   // accpets a selector and returns the first element that matches
-  return document.querySlector(selector);
+  return document.querySelector(selector);
 }
 
 function nestedTarget(){
@@ -19,3 +19,32 @@ function deepestChild(){
   //from div#grand-node
 
 }
+/*
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild(){
+  let node = document.getElementById('grand-node');
+  let next = node.children[0];
+
+  while(next){
+    node = next;
+    next = node.children[0];
+  }
+
+  return node;
+}
+
+function increaseRankBy(n){
+  const l = document.querySelectorAll('ul.ranked-list');
+
+  for (var i = 0; i < l.length; i++) {
+    l[i].innerHTML = parseInt(l[i].innerHTML) + n;
+  }
+}
+*/
