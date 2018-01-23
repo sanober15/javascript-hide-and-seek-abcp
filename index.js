ee8dc7ede20bc05ad0d951407f1399990d1deeda
sanober riaz <sanober@sanober.me>
2018-01-23 23:42:16 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function getFirstSelector(selector){
+  // accpets a selector and returns the first element that matches
+
+}
+
+function nestedTarget(){
+  //pulls a .target out of #nested and .target
+  //# is used for ID Selectors
+
+}
+
+function increaseRankBy(n){
+  //increases the ranks in all of the .ranked-list by n
+
+}
+
+function deepestChild(){
+  //pulls out the most deeply nested children
+  //from div#grand-node
+  
+}
