a525453bba9de3dce9b7db47e142fc5f65d29e83
sanober riaz <sanober@sanober.me>
2018-01-24 00:06:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,27 +35,3 @@ for (var i = 0; i < l.length; i++) {
 }
 return;
 }
-
-/*
-
-
-function deepestChild(){
-  let node = document.getElementById('grand-node');
-  let next = node.children[0];
-
-  while(next){
-    node = next;
-    next = node.children[0];
-  }
-
-  return node;
-}
-
-function increaseRankBy(n){
-  const l = document.querySelectorAll('ul.ranked-list');
-
-  for (var i = 0; i < l.length; i++) {
-    l[i].innerHTML = parseInt(l[i].innerHTML) + n;
-  }
-}
-*/
